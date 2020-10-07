e05b1d0b4f82fe9650e7da629e40a0ab1656d64d
Cak Dion <ajatotindo@gmail.com>
2020-10-07 16:06:38 +0700

    Commit modularisasi package

---
+++ geometri/segitiga.py
@@ -0,0 +1,3 @@
+def menghitung_luas_segitiga(alas, tinggi):
+    luas_segitiga = alas * tinggi / 2
+    return luas_segitiga
---
+++ use_geometry.py
@@ -0,0 +1,3 @@
+# from geometri.segitiga import menghitung_luas_segitiga
+
+print(f"menghitung_luas_segitiga {menghitung_luas_segitiga(10, 3)}")
