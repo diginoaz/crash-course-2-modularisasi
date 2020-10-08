6b2cbb8e12a00517d0c91bff2c74834998b42fec
Cak Dion <ajatotindo@gmail.com>
2020-10-08 13:36:15 +0700

    Commit modularisasi package

--- geometri/segitiga.py
+++ geometri/segitiga.py
@@ -1,3 +1,6 @@
+def info():
+    return 'Modul menghitung rumus-rumus tentang segitiga'
+
 def menghitung_luas_segitiga(alas, tinggi):
     luas_segitiga = alas * tinggi / 2
     return luas_segitiga
--- use_geometry.py
+++ use_geometry.py
@@ -1,3 +1,14 @@
-# from geometri.segitiga import menghitung_luas_segitiga
+from geometri.persegipanjang import hitung_luas_persegi_panjang, info as info_persegi_panjang
+from geometri.segitiga import menghitung_luas_segitiga, info as info_segi_tiga
 
+# import geometri.segitiga as gs
+# print(f"menghitung_luas_segitiga {gs.menghitung_luas_segitiga(10, 3)}")
+
+print(f"\n{info_persegi_panjang()}")
 print(f"menghitung_luas_segitiga {menghitung_luas_segitiga(10, 3)}")
+
+print(f"\n{info_segi_tiga()}")
+print(f"hitung_luas_persegi_panjang {hitung_luas_persegi_panjang(10, 3)}")
+
+
+
