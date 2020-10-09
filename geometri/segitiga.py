5b136154aeb35b2eb22133fe14832701630dfffb
cakdion <ajatotindo@gmail.com>
2020-10-09 17:10:54 +0700

    Commit package external

--- geometri/segitiga.py
+++ geometri/segitiga.py
@@ -1,6 +1,7 @@
 def info():
     return 'Modul menghitung rumus-rumus tentang segitiga'
 
+
 def menghitung_luas_segitiga(alas, tinggi):
     luas_segitiga = alas * tinggi / 2
     return luas_segitiga
